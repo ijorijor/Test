6e9c712762b78ad3373d8dfbc294c86e43a4b7cc
ijorijor <ijor@mail.ru>
2020-11-16 16:42:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-##include "stdio.h"
+#include "stdio.h"
 
 main(int arg, char **argv){
-
+char str[200];
+int n;
 
   for(int i=0;i<150;i++)
     printf("%s\n","Hello Word" );
@@ -9,5 +10,10 @@ main(int arg, char **argv){
   for(int i=0;i<150;i++)
     printf("%s %d \n","Hello Word",i );
 
-}
+    n=0;
+    while (n) {
+      printf("Inpun you name:",);
+      n=scanf(str,"%s");
+    }
 
+}
